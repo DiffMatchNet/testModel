baebf03b407872be6293facd597a84afcfe24a35
DiffMatchNet <108677364+DiffMatchNet@users.noreply.github.com>
2022-07-13 19:06:24 +0100

    Update class123.java

--- src/testgit/class123.java
+++ src/testgit/class123.java
@@ -1,1 +1,36 @@
-//
+package testgit;
+
+public class class123 {
+	public void prepare(MappingManager manager, PreparedStatement ps) {
+        this.session = manager.getSession();
+        this.statement = ps;
+
+        if (method.isVarArgs())
+            throw new IllegalArgumentException(String.format("Invalid varargs method %s in @Accessor interface"));
+        if (ps.getVariables().size() != method.getParameterTypes().length)
+            throw new IllegalArgumentException(String.format("The number of arguments for method %s (%d) does not match the number of bind parameters in the @Query (%d)",
+                                                              method.getName(), method.getParameterTypes().length, ps.getVariables().size()));
+
+
+        Class<?> returnType = method.getReturnType();
+        if (Void.TYPE.isAssignableFrom(returnType) || ResultSet.class.isAssignableFrom(returnType))
+            return;
+        if (Statement.class.isAssignableFrom(returnType)) {
+            returnStatement = true;
+            return;
+        }
+        if (ResultSetFuture.class.isAssignableFrom(returnType)) {
+            this.async = true;
+            return;
+        }
+        if (ListenableFuture.class.isAssignableFrom(returnType)) {
+            this.async = true;
+            Type k = ((ParameterizedType)method.getGenericReturnType()).getActualTypeArguments()[0];
+            if (k instanceof Class && ResultSet.class.isAssignableFrom((Class<?>)k))
+                return;
+            mapType(manager, returnType, k);
+        } else {
+            mapType(manager, returnType, method.getGenericReturnType());
+        }
+    }
+}
