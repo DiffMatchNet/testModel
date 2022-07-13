a544298466cd83f0630eb834ead0685eaa6f21bf
DiffMatchNet <108677364+DiffMatchNet@users.noreply.github.com>
2022-07-13 19:07:15 +0100

    Update class123.java

--- src/testgit/class123.java
+++ src/testgit/class123.java
@@ -5,12 +5,7 @@ public class class123 {
         this.session = manager.getSession();
         this.statement = ps;
 
-        if (method.isVarArgs())
-            throw new IllegalArgumentException(String.format("Invalid varargs method %s in @Accessor interface"));
-        if (ps.getVariables().size() != method.getParameterTypes().length)
-            throw new IllegalArgumentException(String.format("The number of arguments for method %s (%d) does not match the number of bind parameters in the @Query (%d)",
-                                                              method.getName(), method.getParameterTypes().length, ps.getVariables().size()));
-
+      validateParameters();
 
         Class<?> returnType = method.getReturnType();
         if (Void.TYPE.isAssignableFrom(returnType) || ResultSet.class.isAssignableFrom(returnType))
@@ -33,4 +28,23 @@ public class class123 {
             mapType(manager, returnType, method.getGenericReturnType());
         }
     }
-}
+private void validateParameters() {
+        if (method.isVarArgs())
+            throw new IllegalArgumentException(String.format("Invalid varargs method %s in @Accessor interface", method.getName()));
+
+        ColumnDefinitions variables = statement.getVariables();
+        Set<String> names = Sets.newHashSet();
+        for (ColumnDefinitions.Definition variable : variables) {
+            names.add(variable.getName());
+        }
+
+        if (method.getParameterTypes().length < names.size())
+            throw new IllegalArgumentException(String.format("Not enough arguments for method %s, "
+                    + "found %d but it should be at least the number of unique bind parameter names in the @Query (%d)",
+                method.getName(), method.getParameterTypes().length, names.size()));
+
+        if (method.getParameterTypes().length > variables.size())
+            throw new IllegalArgumentException(String.format("Too many arguments for method %s, "
+                    + "found %d but it should be at most the number of bind parameters in the @Query (%d)",
+                method.getName(), method.getParameterTypes().length, variables.size()));
+    }
